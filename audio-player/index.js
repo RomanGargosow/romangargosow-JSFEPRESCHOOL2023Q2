ed1b587894592ffc4cd455ac5514c777de2cc1d2
Roman Gargosow <roman.gargosow@gmail.com>
2023-09-24 18:37:20 +0200

    feat: add audio files

---
+++ audio-player/index.js
@@ -0,0 +1,32 @@
+import { playList } from './playlist';
+const btnPlayTrack = document.querySelector('.play');
+const btnPauseTrack = document.querySelector('.pause');
+const btnNextTrack = document.querySelector('.next');
+const btnPrevTrack = document.querySelector('.prev');
+const trackLine = document.querySelector('.track-line');
+const author = document.querySelectorAll('.name-track');
+const trackTime = document.querySelector('.track-time');
+const cover = document.querySelector('.cover');
+
+// Получите ссылку на аудиоэлемент и кнопку воспроизведения
+const audioPlayer = document.getElementById('audioPlayer');
+const playButton = document.getElementById('playButton');
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const audioPlayer = document.getElementById('audioPlayer');
+  const playButton = document.getElementById('playButton');
+
+  playButton.addEventListener('click', () => {
+    if (audioPlayer.paused) {
+      audioPlayer.play();
+      playButton.innerHTML =
+        '<img src="./source/icons/controls/pause.png" alt="pause">';
+    } else {
+      audioPlayer.pause();
+      playButton.innerHTML =
+        '<img src="./source/icons/controls/play.png" alt="play">';
+    }
+  });
+});
